3e9969b5c3e86df14d1e8098fdf1a42961e2094c
ShotaIuchi <shotaiuchi.develop@gmail.com>
2023-12-15 23:07:19 +0900

    テスト環境構築

---
+++ run_test.sh
@@ -0,0 +1,5 @@
+# 単体テスト＆UIテストの実行
+flutter test
+
+# 結合テストの実行
+./test_integration/run.sh ${pwd}
---
+++ test_integration/run.sh
@@ -0,0 +1,4 @@
+RUN_DIR=${1:-.}
+
+# run integration sample
+flutter drive --driver=${RUN_DIR}/test_integration/driver/driver.dart --target=${RUN_DIR}/test_integration/sample_test.dart
