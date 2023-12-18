7c692c71f5683cf3a812f44edc930d1047a6882a
ShotaIuchi <shotaiuchi.develop@gmail.com>
2023-12-18 17:24:57 +0900

    #012 多言語設定::本対応::追加文言関係::利用箇所

--- lib/ui/screen/search/search_screen.dart
+++ lib/ui/screen/search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_engineer_codecheck/generated/l10n.dart';
 import 'package:flutter_engineer_codecheck/navigation.dart';
 
 /// 検索画面
@@ -14,11 +15,11 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('検索画面')),
+      appBar: AppBar(title: Text(S.of(context).titleSearch)),
       body: Center(
         child: ElevatedButton(
           onPressed: () => naviHandler.navigate('/detail'),
-          child: const Text('詳細画面へ遷移'),
+          child: Text(S.of(context).buttonDetail),
         ),
       ),
     );
